da765297a4794ae2125ad1507a1aa930985f20ef
Victor Olteanu <victor.olteanu@gmail.com>
2013-01-28 10:57:53 -0500

    controller user specs for follow and unfollow - additions

--- spec/controllers/users_controllers_spec.rb
+++ spec/controllers/users_controllers_spec.rb
@@ -12,18 +12,23 @@ describe UsersController do
       to_follow = FactoryGirl.create(:user, :id => 2)
       post :follow, :user_id => 2, format: :json
       subject.current_user.follows?(to_follow).should == true
+    end
 
+    it "doesn't follow anyone and returns errors if invalid user_id was given" do
+      following = subject.current_user.follows
+      post :follow, :user_id => 2, format: :json
+      subject.current_user.follows.should == following 
+      JSON.parse(response.body)['errors'].should == "No Such User"
     end
   end
 
-  describe "POST follow user_id" do 
+  describe "POST unfollow user_id" do 
     login_first_user
     it "can follow the specified user, if a valid user_id was given" do
       to_follow = FactoryGirl.create(:user, :id => 2)
       subject.current_user.follow(to_follow)
       post :unfollow, :user_id => 2, format: :json
       subject.current_user.follows?(to_follow).should == false 
-
     end
   end
 
